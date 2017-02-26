b27ba7e5d7484fccd89a065a032de9274925a4b4
Ben Johnson <johnsonbben@gmail.com>
2017-02-25 16:19:07 -0800

    yeh!

--- js/app.js
+++ js/app.js
@@ -1,4 +1,23 @@
-var videos = [
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+var videos = shuffle([
   {id: "T5Ap6rkyBHY", times: [500, 600]},
   {id: "O5RdMvgk8b0", times: [200, 600]},
   {id: "UpNTwXycNww", times: [230, 600]},
@@ -9,7 +28,7 @@ var videos = [
   {id: "OGFdcaUC6lI", times: [0, 60]},
   {id: "ux7gSKl0Tgw", times: [10, 3600]},
   {id: "R8GlRWPMwFc", times: [10, 45]}
-];
+]);
 
 function pickRandom(array) {
   return array[Math.floor(Math.random() * array.length)];
@@ -42,8 +61,13 @@ function randomOnInterval(min, max) {
     }
   }
 
-  function playRandomVideo() {
-    var video = pickRandom(videos);
+  var nextVideo = 0;
+  function playNextVideo() {
+    if (nextVideo === videos.length) {
+      nextVideo = 0;
+      videos = shuffle(videos);
+    }
+    var video = videos[nextVideo++];
     var time = randomOnInterval(video.times[0], video.times[1]);
     playVideo(video.id, time);
   }
@@ -61,14 +85,14 @@ function randomOnInterval(min, max) {
 
   $(function() {
     // Grab a random video!
-    playRandomVideo();
+    playNextVideo();
     document.body.addEventListener("keypress", function(e) {
       console.log("test");
-      playRandomVideo();
+      playNextVideo();
     });
     setTimeout(function() {
-      $(".pattern").fadeOut(500);
-      $(".big-background-container").fadeOut(500);
+      $(".pattern").fadeOut(1500);
+      $(".big-background-container").fadeOut(1500);
     }, 10000);
   });
 })(jQuery);
